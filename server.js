962ff2f4de79b2102ec75cdb56a101d4258cf277
Jason Thai <jasonthai@Jasons-MacBook-Pro.local>
2016-08-23 21:40:04 -0700

    shes sucks dick

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var app = express();
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+app.set('port', process.env.PORT || 8888);
 
 //serve static files in the 
 app.use('/public', express.static(path.join(__dirname + '/public')));
@@ -13,6 +14,6 @@ app.get('/', function (req, res) {
   	res.render('messages');
 });
 
-app.listen(3000, function () {
-  	console.log('Example app listening on port 3000!');
+var server = app.listen(app.get('port'), function() {
+	console.log('listening on port: ', app.get('port'));
 });
