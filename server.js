20ba02c4f52b47e76077525e1bb668dd1f98d09f
Jason Thai <jasonthai@Jasons-MacBook-Pro.local>
2016-08-30 22:49:04 -0700

    added sockets

--- server.js
+++ server.js
@@ -1,19 +1,97 @@
-var express = require('express');
+var app = require('express')();
 var path = require('path');
 var mysql = require('mysql');
-var app = express();
+var bodyParser = require('body-parser');
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+var pool = mysql.createPool({
+  	host     : 'us-cdbr-iron-east-04.cleardb.net',
+  	user     : 'b813a1e61364f2',
+  	password : '2628f177',
+  	database : 'heroku_f7a2513ae4d0f1b'
+});
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.set('port', process.env.PORT || 8888);
 
-//serve static files in the 
-app.use('/public', express.static(path.join(__dirname + '/public')));
+//serve static files in the public folder
+app.use('/public', require('express').static(path.join(__dirname + '/public')));
 
-app.get('/', function (req, res) {
-  	res.render('messages');
+//middleware for passing data bewteen routes
+app.use(bodyParser());
+
+server.listen(app.get('port'), function() {
+	console.log('listening on port:', app.get('port'));
 });
 
-var server = app.listen(app.get('port'), function() {
-	console.log('listening on port: ', app.get('port'));
+app.get('/', function (req, res) {
+  	
+  	pool.getConnection(function(err,connection) {
+  		connection.query('SELECT * FROM messages;', function(err, rows) {
+  			var allMessages = [];
+  			
+  			if (err) {
+				console.log(err);
+			} else {
+
+				for(var i = 0; i < rows.length; i++) {
+					var row = rows[i];
+					allMessages.push(row.message);
+
+				}
+			}
+
+			connection.release();
+
+			var messageObj = {'allMessages': allMessages};
+
+			res.render('messages', messageObj);
+  		});
+  	});
+});
+
+// app.post('/send-message', function(req, res) {
+// 	var message = req.body.message;
+	
+// 	pool.getConnection(function(err, connection) {
+// 		connection.query('INSERT INTO messages (message) VALUES ("' + message + '")  ;', function(err, rows) {
+			
+// 			if (err) {
+// 				console.log(err);
+// 			} else {
+// 				console.log(rows);
+// 			}
+			
+// 			connection.release();
+// 			res.redirect('/');
+// 		});
+// 	});
+// });
+
+io.on('connection', function(socket){
+	
+	socket.on('send-message', function(data) {
+	    var message = data.message;
+
+	    pool.getConnection(function(err, connection) {
+			connection.query('INSERT INTO messages (message) VALUES ("' + message + '")  ;', function(err, rows) {
+				
+				if (err) {
+					console.log(err);
+				} else {
+					console.log(rows);
+				}
+				
+				connection.release();
+
+				io.emit('update-message-area', {'message': message});
+			});
+		});
+	});
+  	
+  	socket.on('disconnect', function(){
+
+  	});
 });
